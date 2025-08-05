34f2ceb8d3c6f734d241bd857da2c721e5f19406
yahlimazri <yahlimazri@gmail.com>
2025-08-05 16:36:26 +0300

    Delete ProductFactory.java

--- ProductFactory.java
+++
@@ -1,7 +0,0 @@
-package ShaharAndYahli;
-
-public class ProductFactory {
-    public static Product createProduct(String name, double price, ProductCategory category, boolean specialPackaging, double packagingPrice, String sellerUsername) {
-        return new Product(name, price, category, specialPackaging, packagingPrice, sellerUsername);
-    }
-}
