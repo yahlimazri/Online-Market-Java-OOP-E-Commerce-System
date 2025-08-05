d588695779547a6683e6417438eaa260d29401ec
yahlimazri <yahlimazri@gmail.com>
2025-08-05 16:37:50 +0300

    Delete OrderFactory.java

--- OrderFactory.java
+++
@@ -1,9 +0,0 @@
-package ShaharAndYahli;
-
-import java.util.Date;
-
-public class OrderFactory {
-    public static Order createOrder(Product[] products, Date date, double totalPrice) {
-        return new Order(products, date, totalPrice);
-    }
-}
