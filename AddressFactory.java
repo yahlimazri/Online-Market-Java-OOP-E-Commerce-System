6eb332238ad65a7670851bcf69bb00ce8a2dc304
yahlimazri <yahlimazri@gmail.com>
2025-08-05 16:32:21 +0300

    Delete AddressFactory.java

--- AddressFactory.java
+++
@@ -1,7 +0,0 @@
-package ShaharAndYahli;
-
-public class AddressFactory {
-    public static Address createAddress(String streetName, String buildingNumber, String city, String state) {
-        return new Address(streetName, buildingNumber, city, state);
-    }
-}
