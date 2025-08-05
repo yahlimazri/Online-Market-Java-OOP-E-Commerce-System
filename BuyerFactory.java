1c44cf0fd9a251aa2f7db23c6488a706416bba7b
yahlimazri <yahlimazri@gmail.com>
2025-08-05 16:35:23 +0300

    Delete BuyerFactory.java

--- BuyerFactory.java
+++
@@ -1,7 +0,0 @@
-package ShaharAndYahli;
-
-public class BuyerFactory {
-    public static Buyer createBuyer(String username, String password, Address address) {
-        return new Buyer(username, password, address);
-    }
-}
