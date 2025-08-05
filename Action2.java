59fb1814292b552f3711efefe193e50cbdf3c1b7
yahlimazri <yahlimazri@gmail.com>
2025-08-05 15:57:49 +0300

    Delete Action2.java

--- Action2.java
+++
@@ -1,8 +0,0 @@
-package ShaharAndYahli;
-
-public class Action2 implements IterationListener {
-    @Override
-    public void onIterationEnded(String message) {
-        System.out.println("Action2 got: " + message);
-    }
-}
